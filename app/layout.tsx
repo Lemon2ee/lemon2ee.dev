d7d457afd445b7ec601cd67c1f8fd6de1df5bdf3
Sebastian Wang <43295730+Lemon2ee@users.noreply.github.com>
2023-12-23 05:20:06 -0500

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
