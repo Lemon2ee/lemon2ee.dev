ab835e94bc8c73bc29817cf1e801f1a1e3009061
Sebastian Wang <43295730+Lemon2ee@users.noreply.github.com>
2023-12-23 08:46:02 -0500

    should be MDXcontent

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import dynamic from "next/dynamic";
+import { MDXProps } from "mdx/types";
+import React from "react";
 
 export default async function Blog({
   params,
@@ -8,11 +10,18 @@ export default async function Blog({
   };
 }) {
   const blogPostName = params.slug;
-  const Blog = dynamic(() => import("@/posts/" + blogPostName + ".mdx"), {
-    ssr: true,
-  });
-
-  console.log(Blog);
+  const Blog: React.ComponentType<MDXProps> = dynamic(
+    () =>
+      import("@/posts/" + blogPostName + ".mdx")
+        .then((mod) => mod.default)
+        .then((mod) => {
+          console.log(mod);
+          return mod;
+        }),
+    {
+      ssr: true,
+    },
+  );
 
   return (
     <article className={"prose pt-4 prose-custom dark:prose-invert"}>
