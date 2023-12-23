bdee563914f02995bd2e81bd89604bea62ce38de
Sebastian Wang <43295730+Lemon2ee@users.noreply.github.com>
2023-12-23 06:45:03 -0500

    log test

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -8,9 +8,12 @@ export default async function Blog({
   };
 }) {
   const blogPostName = params.slug;
+  console.log("about to import the blog component");
   const Blog = dynamic(() => import("@/posts/" + blogPostName + ".mdx"), {
     loading: () => <p>Loading...</p>,
   });
+  console.log("finished loading");
+
   return (
     <article className={"prose pt-4 prose-custom dark:prose-invert"}>
       <Blog />
