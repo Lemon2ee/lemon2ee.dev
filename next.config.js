8ab238f84d6846e01fc2636056abf674da9885e7
Sebastian Wang <43295730+Lemon2ee@users.noreply.github.com>
2023-12-23 07:28:59 -0500

    why am i even awake at 7?

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
+  experimental: {
+    outputFileTracingIncludes: {
+      "/blog/[slug]": ["./posts/*"],
+    },
+  },
 };
 
 module.exports = withMDX(nextConfig);
