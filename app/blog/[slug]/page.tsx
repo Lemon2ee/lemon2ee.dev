9acc04f7bd2f7943eb9e46f25995ee0ef23d3b00
Sebastian Wang <43295730+Lemon2ee@users.noreply.github.com>
2023-12-23 07:22:47 -0500

    i hate myself

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -9,9 +9,12 @@ export default async function Blog({
 }) {
   const blogPostName = params.slug;
   console.log("about to import the blog component");
-  const Blog = dynamic(() => import("@/posts/" + blogPostName + ".mdx"), {
-    ssr: false,
-  });
+  const Blog = dynamic(
+    () => import("@/posts/" + blogPostName + ".mdx").then((mod) => mod.default),
+    {
+      ssr: true,
+    },
+  );
 
   return (
     <article className={"prose pt-4 prose-custom dark:prose-invert"}>
