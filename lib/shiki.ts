63bda04153ec97ff833c38629fc5391d68207d7f
Sebastian Wang <43295730+Lemon2ee@users.noreply.github.com>
2024-07-01 03:53:20 -0400

    added log

--- lib/shiki.ts
+++ lib/shiki.ts
@@ -5,12 +5,14 @@ let highlighter: Highlighter;
 
 export async function highlight(code: string, lang: string) {
   if (!highlighter) {
+    console.log("Initializing highlighter")
     highlighter = await getHighlighter({
       langs: Object.keys(bundledLanguages),
       themes: ["dracula", "solarized-light"],
     });
   }
 
+  console.log("Starting to highlight code")
   return highlighter.codeToHtml(code, {
     lang: lang,
     themes: {
