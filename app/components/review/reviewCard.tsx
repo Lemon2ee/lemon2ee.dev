d44b82ec47a01cf16ad661f5fb0a6aa325f6258e
Sebastian Wang <wang.yizho@northeastern.edu>
2024-07-02 16:30:45 -0400

    insert html

--- app/components/review/reviewCard.tsx
+++ app/components/review/reviewCard.tsx
@@ -24,23 +24,23 @@ export function ReviewCard({
                 alt="Modern building architecture"
             />
           </div>
-          <div className="p-8">
-            <div className="uppercase tracking-wide text-xl text-indigo-500 font-semibold">
-              {title}
-            </div>
-            <div className="row-span-1 flex items-center w-3/5 py-4">
+            <div className="p-8">
+                <div className="uppercase tracking-wide text-xl text-indigo-500 font-semibold">
+                    {title}
+                </div>
+                <div className="row-span-1 flex items-center w-3/5 py-4">
             <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
               {roundedRating}
             </span>
-              <div className="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700 ms-2">
-                <div
-                    className="bg-blue-600 h-2.5 rounded dark:bg-blue-500"
-                    style={{ width: widthPercentage }}
-                ></div>
-              </div>
+                    <div className="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700 ms-2">
+                        <div
+                            className="bg-blue-600 h-2.5 rounded dark:bg-blue-500"
+                            style={{width: widthPercentage}}
+                        ></div>
+                    </div>
+                </div>
+                <p className="text-neutral-100" dangerouslySetInnerHTML={{__html: content}}></p>
             </div>
-            <p className="text-neutral-100">{content}</p>
-          </div>
         </div>
       </div>
   );
