dc1fc9145f3e977fedb8f4ac8ea0d7a054d03e67
Sebastian Wang <wang.yizho@northeastern.edu>
2024-07-03 19:36:24 -0400

    nav bar highlight

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,8 +1,11 @@
+"use client"
+
 import Link from "next/link";
+import { usePathname } from 'next/navigation'
 
 const navItems = {
   "/": {
-    name: "home",
+    name: "about me",
   },
   "/profile": {
     name: "profile",
@@ -19,28 +22,37 @@ const navItems = {
 };
 
 export function Navbar() {
+  const currentPath = usePathname();
+  const pathSegments = currentPath.split('/').filter(segment => segment !== '');
+  // Get the first segment, or an empty string if there are no segments
+  const firstPathName = pathSegments.length > 0 ? pathSegments[0] : '';
+
   return (
-    <aside className="-ml-[8px] mb-10 tracking-tight">
-      <div className="lg:sticky lg:top-20">
-        <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
-          id="nav"
-        >
-          <div className="flex flex-row space-x-0 pr-10">
-            {Object.entries(navItems).map(([path, { name }]) => {
-              return (
-                <Link
-                  key={path}
-                  href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2"
-                >
-                  {name}
-                </Link>
-              );
-            })}
-          </div>
-        </nav>
-      </div>
-    </aside>
+      <aside className="-ml-[8px] mb-10 tracking-tight">
+        <div className="lg:sticky lg:top-20">
+          <nav
+              className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+              id="nav"
+          >
+            <div className="flex flex-row space-x-0 pr-10">
+              {Object.entries(navItems).map(([path, { name }]) => {
+                console.log(firstPathName)
+                console.log(path)
+                return (
+                    <Link
+                        key={path}
+                        href={path}
+                        className={
+                          `transition-all flex align-middle relative py-1 px-2 ${"/" + firstPathName === path ? 'text-cyan-200' : ''}`
+                        }
+                    >
+                      {name}
+                    </Link>
+                );
+              })}
+            </div>
+          </nav>
+        </div>
+      </aside>
   );
 }
