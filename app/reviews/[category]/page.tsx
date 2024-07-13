1f9840bc6adbdb1ea1d49598925a0822ffb0915d
Sebastian Wang <43295730+Lemon2ee@users.noreply.github.com>
2024-07-13 00:52:20 -0400

    some info update

--- app/reviews/[category]/page.tsx
+++ app/reviews/[category]/page.tsx
@@ -33,6 +33,7 @@ export default async function ReviewPage({ params }: Props) {
                             src={headerImgUrl}
                             alt={review.title}
                             style={{objectFit:"cover"}}
+                            className={"rounded-md"}
                         />
                     </Link>
                 );
