3cc45849082a19f1abaca31157cb1fcafd631926
Sebastian Wang <43295730+Lemon2ee@users.noreply.github.com>
2024-09-02 11:26:31 +0800

    Update Quote.tsx

--- app/components/mdx/Quote.tsx
+++ app/components/mdx/Quote.tsx
@@ -69,8 +69,7 @@ function extractAndCapitalize(text: any): string {
 }
 
 const CustomQuote = ({ children }: any) => {
-  const messageType = children[1]?.props?.children[0];
-  console.log(children[1]?.props?);
+  const messageType = children[1]?.props?.children[0] ?? "[!TIP]";
   const { borderClassName, textClassName, component } =
     // @ts-ignore
     messageStyles[messageType];
